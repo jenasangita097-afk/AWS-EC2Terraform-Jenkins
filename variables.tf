b5e037140a9445be6dffbb12f2af1652ee7f18ba
jenasangita097-afk <jenasangita097@gmail.com>
2026-04-01 10:12:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,7 @@
-variable "region" {
-  description = "AWS region"
-  default     = "ap-south-1"
+variable "aws_region" {
+  default = "ap-south-1"
 }
-
-variable "instance_count" {
-  description = "Number of EC2 instances"
-  default     = 2
-}
-
+ 
 variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t3.micro"
-}
-
-variable "ami_id" {
-  description = "AMI ID for Amazon Linux"
-  default     = "ami-0f58b397bc5c1f2e8"
-}
-
-variable "instance_name" {
-  description = "Base name for instances"
-  default     = "server"
+  default = "t3.micro"
 }
